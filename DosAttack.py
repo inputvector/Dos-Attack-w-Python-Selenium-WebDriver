c474fed4c185dc1422720e58a088b056c2c16699
Bekir Dabanoglu <65566906+inputvector@users.noreply.github.com>
2021-12-04 01:42:00 -0800

    Update DosAttack.py

--- DosAttack.py
+++ DosAttack.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 import datetime
 import sys
